5f87e1ff92e5900eaedef693348a6f4566712d42
Tofik Mussa <tofahub@gmail.com>
2018-06-12 16:43:58 -0400

    Arraylist

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String>movies = new ArrayList<>();
+
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your favorite movies and Q to quit: ");
+            String keyboard = input.next();
+
+            if(keyboard.charAt(0) == 'Q'){
+                break;
+            }
+
+            movies.add(keyboard);
+        }
+
+        for(String movie: movies){
+            System.out.println(movie);
+        }
+    }
+}
